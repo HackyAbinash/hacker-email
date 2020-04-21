a284299368acbf5dcabfad5f5f950712a0ad5f85
HackyAbinash <64101711+HackyAbinash@users.noreply.github.com>
2020-04-22 02:42:51 +0530

    Update hacker-email.py

--- hacker-email.py
+++ hacker-email.py
@@ -1,12 +1,9 @@
 # thanks to github.com/thelinuxchoice for helping
-import requests
-print("                   
-print("   
+import requests 
 print("╦ ╦┌─┐┌─┐┬┌─┌─┐┬─┐  ┌─┐┌┬┐┌─┐┬┬  ")
 print("╠═╣├─┤│  ├┴┐├┤ ├┬┘  ├┤ │││├─┤││   ")
 print("╩ ╩┴ ┴└─┘┴ ┴└─┘┴└─  └─┘┴ ┴┴ ┴┴┴─┘ ")
-print("   
-print("                                    
+                              
 
 print(" \nAnonymous Email by anonymouse.org ")
 print("Coded by \033[91m github.com/HackyAbinash \033[0m \n ")
