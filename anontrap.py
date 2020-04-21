cc608f6f0582f00d9652fc3f4e7a77fa312e1278
HackyAbinash <64101711+HackyAbinash@users.noreply.github.com>
2020-04-22 03:15:45 +0530

    Update anontrap.py

--- anontrap.py
+++ anontrap.py
@@ -1,5 +1,5 @@
 # thanks to github.com/thelinuxchoice for helping 
-
+import requests
 print(" \nAnonymous Email by anonymouse.org ")
 print("Coded by \033[91m github.com/HackyAbinash \033[0m \n ")
 to = input("To: ")
